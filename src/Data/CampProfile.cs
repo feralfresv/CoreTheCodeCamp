fdeb08831c995e976d53c9e259d2a8da2bc9315c
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-03-27 17:24:56 -0600

    first commit

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,19 @@
+﻿using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile :Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+            .ForMember(cm => cm.Venue, o => o.MapFrom(m => m.Location.VenueName));
+        }
+    }
+}
